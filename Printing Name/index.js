16df40329aaf313efc76ec73f1f0a60c81fd2bcd
AhmadTijani <ahmadtijani84@gmail.com>
2021-08-19 15:40:05 +0100

    first commit

---
+++ Printing Name/index.js
@@ -0,0 +1,13 @@
+/*System.out.println("AhmadTijani");*/
+
+let user = {
+    name: 'Ramoni Tijani',
+}
+  console.log(user)
+
+  const myName = 'Ramoni Tijani'
+Object.keys({myName})[0]
+
+console.log(myName)
+
+// returns "myFirstName"
